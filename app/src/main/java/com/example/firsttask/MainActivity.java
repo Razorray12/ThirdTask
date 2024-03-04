5280389ea2373091e0abe32eb8a3580c55b4c165
Denis <den.potemkin2015@mail.ru>
2024-03-04 03:37:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firsttask/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.firsttask;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    public final static String EXTRA_MESSAGE = "com.itproger.firstprak.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View v){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText emailEditText = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        EditText passwordEditText = (EditText) findViewById(R.id.editTextTextPassword);
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String message = email + "\n"+ password;
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+        Toast.makeText(this, "Вы вошли!", Toast.LENGTH_SHORT).show();
+    }
+}
